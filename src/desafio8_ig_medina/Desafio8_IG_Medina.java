554d404c4ae52806b4d08da5b6a2f820601c8bff
Federico Medina <fedemedina05@gmail.com>
2025-10-09 09:37:00 -0300

    Formulario del desafio 8

---
+++ src/desafio8_ig_medina/Desafio8_IG_Medina.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package desafio8_ig_medina;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author fedem
+ */
+public class Desafio8_IG_Medina {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
